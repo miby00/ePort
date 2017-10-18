13673442f6d51a46192156e49c0e9ed6bf49ea9f
Taddic <tommy.mattsson@gmail.com>
2017-10-18 16:22:53 +0200

    Added some comments and shutting down the ePorts using ePort functions instead

--- examples/MultipleServerModules/exampleMultiple.erl
+++ examples/MultipleServerModules/exampleMultiple.erl
@@ -26,14 +26,19 @@ start() ->
     {ok,SrvPid}   = ePortListener:start_link(listProt, Port),
     {ok,ListPid}  = ePort:start_link({undefined,listProt},"localhost",Port),
     {ok,RandPid1} = ePort:start_link({undefined,randProt},"localhost",Port),
-
     ePortListener:updateModulesAllowed(SrvPid, [listProt, randProt]),
     {ok,RandPid2} = ePort:start_link({undefined,randProt},"localhost",Port),
 
+    %% Expected value: [1,2,3,4]
     io:format("ListPid: ~p~n", [ePort:call(ListPid,append,[[1,2],[3,4]])]),
+
+    %% Expected value: {error, timeout}
     io:format("RandPid1: ~p~n", [ePort:call(RandPid1, uniform, [1000])]),
+
+    %% Expected value: random number between 1 and 1000
     io:format("RandPid2: ~p~n", [ePort:call(RandPid2, uniform, [1000])]),
-    exit(ListPid,  kill),
-    exit(RandPid1,  kill),
-    exit(RandPid2, kill),
+    ePort:stop(ListPid),
+    ePort:stop(RandPid1),
+    ePort:stop(RandPid2),
+    ePortListener:stop(SrvPid),
     ok.
